e7221724c48cfbfdc1ec2087946356fa85de120f
Nacim Goura <nacim.goura.dev@gmail.com>
2018-10-18 16:13:59 +0200

    add new field for IT (old institute)

--- src/views/Instituts.js
+++ src/views/Instituts.js
@@ -7,14 +7,7 @@ import {
   List,
   Filter,
   SimpleForm,
-  ReferenceArrayField,
-  SingleFieldList,
-  ChipField,
-  TextInput,
-  ReferenceArrayInput,
-  SelectArrayInput,
-  ReferenceInput,
-  AutocompleteInput
+  TextInput
 } from "react-admin";
 import DeleteButtonWithConfirmation from "../components/DeleteButtonWithConfirmation";
 import LinkEdit from "../components/LinkEdit";
@@ -24,16 +17,7 @@ const InstitutsFilter = props => (
   <Filter {...props}>
     <TextInput label="Rechercher" source="match" alwaysOn />
     <TextInput source="id" label="resources.institutes.fields.id" />
-    <TextInput source="like_institute.code" label="resources.institutes.fields.code" />
     <TextInput source="like_institute.name" label="resources.institutes.fields.name" />
-    <ReferenceInput
-      label="resources.institutes.fields.communities"
-      source="community.id"
-      reference="communities"
-      perPage={100}
-    >
-      <AutocompleteInput optionText="name" />
-    </ReferenceInput>
   </Filter>
 );
 
@@ -41,17 +25,7 @@ export const InstitutsList = ({ ...props }) => (
   <List {...props} filters={<InstitutsFilter />} perPage={10}>
     <Datagrid>
       <LinkEdit source="id" label="resources.institutes.fields.id" />
-      <LinkEdit label="resources.institutes.fields.code" source="code" />
       <LinkEdit source="name" label="resources.institutes.fields.name" />
-      <ReferenceArrayField
-        label="resources.institutes.fields.communities"
-        reference="communities"
-        source="communities"
-      >
-        <SingleFieldList>
-          <ChipField source="name" />
-        </SingleFieldList>
-      </ReferenceArrayField>
       <EditButton />
       <DeleteButtonWithConfirmation />
     </Datagrid>
@@ -65,17 +39,12 @@ const InstitutsTitle = ({ record }) => {
 export const InstitutsEdit = ({ ...props }) => (
   <Edit title={<InstitutsTitle />} {...props} actions={<ListActions />}>
     <SimpleForm>
-      <TextInput source="id" label="resources.institutes.fields.id" />
-      <TextInput source="code" label="resources.institutes.fields.code" />
       <TextInput source="name" label="resources.institutes.fields.name" />
-      <ReferenceArrayInput
-        label="resources.institutes.fields.communities"
-        reference="communities"
-        source="communities"
-        className="tags"
-      >
-        <SelectArrayInput source="name" />
-      </ReferenceArrayInput>
+      <TextInput source="address" label="resources.institutes.fields.address" />
+      <TextInput source="phone" label="resources.institutes.fields.phone" />
+      <TextInput source="manager" label="resources.institutes.fields.manager" />
+      <TextInput source="mail_manager" label="resources.institutes.fields.mail_manager" />
+      <TextInput source="dr" label="resources.institutes.fields.dr" />
     </SimpleForm>
   </Edit>
 );
@@ -83,17 +52,12 @@ export const InstitutsEdit = ({ ...props }) => (
 export const InstitutsCreate = ({ ...props }) => (
   <Create {...props} actions={<ListActions />}>
     <SimpleForm redirect="list">
-      <TextInput source="id" label="resources.institutes.fields.id" />
-      <TextInput source="code" label="resources.institutes.fields.code" />
       <TextInput source="name" label="resources.institutes.fields.name" />
-      <ReferenceArrayInput
-        label="resources.institutes.fields.communities"
-        reference="communities"
-        source="communities"
-        className="tags"
-      >
-        <SelectArrayInput source="name" />
-      </ReferenceArrayInput>
+      <TextInput source="address" label="resources.institutes.fields.address" />
+      <TextInput source="phone" label="resources.institutes.fields.phone" />
+      <TextInput source="manager" label="resources.institutes.fields.manager" />
+      <TextInput source="mail_manager" label="resources.institutes.fields.mail_manager" />
+      <TextInput source="dr" label="resources.institutes.fields.dr" />
     </SimpleForm>
   </Create>
 );
