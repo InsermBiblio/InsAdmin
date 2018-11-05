b172ac0f5c3e70622988afc4965178804c6284e2
Math <mathieu.risakota@gmail.com>
2018-11-05 11:45:39 +0100

    Add views Structures and Teams
    Edit and Create

--- src/views/Teams.js
+++ src/views/Teams.js
@@ -9,6 +9,7 @@ import {
   SimpleForm,
   SingleFieldList,
   TextInput,
+  BooleanInput,
   ReferenceArrayInput,
   SelectArrayInput,
   ReferenceArrayField,
@@ -62,9 +63,12 @@ const TeamsTitle = ({ record }) => {
 export const TeamsEdit = ({ ...props }) => (
   <Edit title={<TeamsTitle />} {...props}>
     <SimpleForm>
+      <TextInput label="Rechercher" source="match" alwaysOn />
       <TextInput source="title" label="resources.revues.fields.title" />
-      <TextInput source="url" label="resources.revues.fields.url" />
-
+      <BooleanInput
+        source="active"
+        label="resources.structures.fields.active"
+      />
       <ReferenceArrayInput
         label="resources.revues.fields.communities"
         reference="communities"
@@ -74,6 +78,156 @@ export const TeamsEdit = ({ ...props }) => (
           <ChipField source="name" />
         </SelectArrayInput>
       </ReferenceArrayInput>
+      <TextInput source="site" label="resources.revues.fields.site" />
+      <TextInput
+        source="team_number"
+        label="resources.revues.fields.team_number"
+      />
+      <TextInput
+        source="principal_lastname"
+        label="resources.revues.fields.principal_lastname"
+      />
+      <TextInput
+        source="principal_firstname"
+        label="resources.revues.fields.principal_firstname"
+      />
+      <TextInput
+        source="principal_email"
+        label="resources.revues.fields.principal_email"
+      />
+      <TextInput
+        source="nb_researchers_inserm_pp"
+        label="resources.revues.fields.nb_researchers_inserm_pp"
+      />
+      <TextInput
+        source="nb_researchers_inserm_etp"
+        label="resources.revues.fields.nb_researchers_inserm_etp"
+      />
+      <TextInput
+        source="nb_researchers_crns_pp"
+        label="resources.revues.fields.nb_researchers_crns_pp"
+      />
+      <TextInput
+        source="nb_researchers_crns_etp"
+        label="resources.revues.fields.nb_researchers_crns_etp"
+      />
+      <TextInput
+        source="nb_researchers_other_pp"
+        label="resources.revues.fields.nb_researchers_other_pp"
+      />
+      <TextInput
+        source="nb_researchers_other_etp"
+        label="resources.revues.fields.nb_researchers_other_etp"
+      />
+      <TextInput
+        source="nb_post_phd_student_pp"
+        label="resources.revues.fields.nb_post_phd_student_pp"
+      />
+      <TextInput
+        source="nb_post_phd_student_etp"
+        label="resources.revues.fields.nb_post_phd_student_etp"
+      />
+      <TextInput
+        source="nb_phd_student_pp"
+        label="resources.revues.fields.nb_phd_student_pp"
+      />
+      <TextInput
+        source="nb_phd_student_etp"
+        label="resources.revues.fields.nb_phd_student_etp"
+      />
+      <TextInput
+        source="nb_cdi_researchers_pp"
+        label="resources.revues.fields.nb_cdi_researchers_pp"
+      />
+      <TextInput
+        source="nb_cdi_researchers_etp"
+        label="resources.revues.fields.nb_cdi_researchers_etp"
+      />
+      <TextInput
+        source="nb_cdd_researchers_pp"
+        label="resources.revues.fields.nb_cdd_researchers_pp"
+      />
+      <TextInput
+        source="nb_cdd_researchers_etp"
+        label="resources.revues.fields.nb_cdd_researchers_etp"
+      />
+      <TextInput
+        source="nb_teacher_researchers_pp"
+        label="resources.revues.fields.nb_teacher_researchers_pp"
+      />
+      <TextInput
+        source="nb_teacher_researchers_etp"
+        label="resources.revues.fields.nb_teacher_researchers_etp"
+      />
+      <TextInput
+        source="nb_pu_ph_pp"
+        label="resources.revues.fields.nb_pu_ph_pp"
+      />
+      <TextInput
+        source="nb_pu_ph_etp"
+        label="resources.revues.fields.nb_pu_ph_etp"
+      />
+      <TextInput
+        source="nb_hosp_others_pp"
+        label="resources.revues.fields.nb_hosp_others_pp"
+      />
+      <TextInput
+        source="nb_hosp_others_etp"
+        label="resources.revues.fields.nb_hosp_others_etp"
+      />
+      <TextInput
+        source="nb_ir_inserm_pp"
+        label="resources.revues.fields.nb_ir_inserm_pp"
+      />
+      <TextInput
+        source="nb_ir_inserm_etp"
+        label="resources.revues.fields.nb_ir_inserm_etp"
+      />
+      <TextInput
+        source="nb_ir_non_inserm_pp"
+        label="resources.revues.fields.nb_ir_non_inserm_pp"
+      />
+      <TextInput
+        source="nb_ir_non_inserm_etp"
+        label="resources.revues.fields.nb_ir_non_inserm_etp"
+      />
+      <TextInput
+        source="nb_ita_others_pp"
+        label="resources.revues.fields.nb_ita_others_pp"
+      />
+      <TextInput
+        source="nb_ita_others_etp"
+        label="resources.revues.fields.nb_ita_others_etp"
+      />
+      <TextInput
+        source="nb_cdd_ir_pp"
+        label="resources.revues.fields.nb_cdd_ir_pp"
+      />
+      <TextInput
+        source="nb_cdd_ir_etp"
+        label="resources.revues.fields.nb_cdd_ir_etp"
+      />
+      <TextInput
+        source="nb_cdd_others_pp"
+        label="resources.revues.fields.nb_cdd_others_pp"
+      />
+      <TextInput
+        source="nb_cdd_others_etp"
+        label="resources.revues.fields.nb_cdd_others_etp"
+      />
+      <TextInput
+        source="nb_admin_pp"
+        label="resources.revues.fields.nb_admin_pp"
+      />
+      <TextInput
+        source="nb_admin_etp"
+        label="resources.revues.fields.nb_admin_etp"
+      />
+      <TextInput
+        source="nb_personal_accounts"
+        label="resources.revues.fields.nb_personal_accounts"
+      />
+      <TextInput source="comment" label="resources.revues.fields.comment" />
     </SimpleForm>
   </Edit>
 );
@@ -81,9 +235,12 @@ export const TeamsEdit = ({ ...props }) => (
 export const TeamsCreate = ({ ...props }) => (
   <Create {...props}>
     <SimpleForm redirect="list">
+      <TextInput label="Rechercher" source="match" alwaysOn />
       <TextInput source="title" label="resources.revues.fields.title" />
-      <TextInput source="url" label="resources.revues.fields.url" />
-
+      <BooleanInput
+        source="active"
+        label="resources.structures.fields.active"
+      />
       <ReferenceArrayInput
         label="resources.revues.fields.communities"
         reference="communities"
@@ -93,6 +250,156 @@ export const TeamsCreate = ({ ...props }) => (
           <ChipField source="name" />
         </SelectArrayInput>
       </ReferenceArrayInput>
+      <TextInput source="site" label="resources.revues.fields.site" />
+      <TextInput
+        source="team_number"
+        label="resources.revues.fields.team_number"
+      />
+      <TextInput
+        source="principal_lastname"
+        label="resources.revues.fields.principal_lastname"
+      />
+      <TextInput
+        source="principal_firstname"
+        label="resources.revues.fields.principal_firstname"
+      />
+      <TextInput
+        source="principal_email"
+        label="resources.revues.fields.principal_email"
+      />
+      <TextInput
+        source="nb_researchers_inserm_pp"
+        label="resources.revues.fields.nb_researchers_inserm_pp"
+      />
+      <TextInput
+        source="nb_researchers_inserm_etp"
+        label="resources.revues.fields.nb_researchers_inserm_etp"
+      />
+      <TextInput
+        source="nb_researchers_crns_pp"
+        label="resources.revues.fields.nb_researchers_crns_pp"
+      />
+      <TextInput
+        source="nb_researchers_crns_etp"
+        label="resources.revues.fields.nb_researchers_crns_etp"
+      />
+      <TextInput
+        source="nb_researchers_other_pp"
+        label="resources.revues.fields.nb_researchers_other_pp"
+      />
+      <TextInput
+        source="nb_researchers_other_etp"
+        label="resources.revues.fields.nb_researchers_other_etp"
+      />
+      <TextInput
+        source="nb_post_phd_student_pp"
+        label="resources.revues.fields.nb_post_phd_student_pp"
+      />
+      <TextInput
+        source="nb_post_phd_student_etp"
+        label="resources.revues.fields.nb_post_phd_student_etp"
+      />
+      <TextInput
+        source="nb_phd_student_pp"
+        label="resources.revues.fields.nb_phd_student_pp"
+      />
+      <TextInput
+        source="nb_phd_student_etp"
+        label="resources.revues.fields.nb_phd_student_etp"
+      />
+      <TextInput
+        source="nb_cdi_researchers_pp"
+        label="resources.revues.fields.nb_cdi_researchers_pp"
+      />
+      <TextInput
+        source="nb_cdi_researchers_etp"
+        label="resources.revues.fields.nb_cdi_researchers_etp"
+      />
+      <TextInput
+        source="nb_cdd_researchers_pp"
+        label="resources.revues.fields.nb_cdd_researchers_pp"
+      />
+      <TextInput
+        source="nb_cdd_researchers_etp"
+        label="resources.revues.fields.nb_cdd_researchers_etp"
+      />
+      <TextInput
+        source="nb_teacher_researchers_pp"
+        label="resources.revues.fields.nb_teacher_researchers_pp"
+      />
+      <TextInput
+        source="nb_teacher_researchers_etp"
+        label="resources.revues.fields.nb_teacher_researchers_etp"
+      />
+      <TextInput
+        source="nb_pu_ph_pp"
+        label="resources.revues.fields.nb_pu_ph_pp"
+      />
+      <TextInput
+        source="nb_pu_ph_etp"
+        label="resources.revues.fields.nb_pu_ph_etp"
+      />
+      <TextInput
+        source="nb_hosp_others_pp"
+        label="resources.revues.fields.nb_hosp_others_pp"
+      />
+      <TextInput
+        source="nb_hosp_others_etp"
+        label="resources.revues.fields.nb_hosp_others_etp"
+      />
+      <TextInput
+        source="nb_ir_inserm_pp"
+        label="resources.revues.fields.nb_ir_inserm_pp"
+      />
+      <TextInput
+        source="nb_ir_inserm_etp"
+        label="resources.revues.fields.nb_ir_inserm_etp"
+      />
+      <TextInput
+        source="nb_ir_non_inserm_pp"
+        label="resources.revues.fields.nb_ir_non_inserm_pp"
+      />
+      <TextInput
+        source="nb_ir_non_inserm_etp"
+        label="resources.revues.fields.nb_ir_non_inserm_etp"
+      />
+      <TextInput
+        source="nb_ita_others_pp"
+        label="resources.revues.fields.nb_ita_others_pp"
+      />
+      <TextInput
+        source="nb_ita_others_etp"
+        label="resources.revues.fields.nb_ita_others_etp"
+      />
+      <TextInput
+        source="nb_cdd_ir_pp"
+        label="resources.revues.fields.nb_cdd_ir_pp"
+      />
+      <TextInput
+        source="nb_cdd_ir_etp"
+        label="resources.revues.fields.nb_cdd_ir_etp"
+      />
+      <TextInput
+        source="nb_cdd_others_pp"
+        label="resources.revues.fields.nb_cdd_others_pp"
+      />
+      <TextInput
+        source="nb_cdd_others_etp"
+        label="resources.revues.fields.nb_cdd_others_etp"
+      />
+      <TextInput
+        source="nb_admin_pp"
+        label="resources.revues.fields.nb_admin_pp"
+      />
+      <TextInput
+        source="nb_admin_etp"
+        label="resources.revues.fields.nb_admin_etp"
+      />
+      <TextInput
+        source="nb_personal_accounts"
+        label="resources.revues.fields.nb_personal_accounts"
+      />
+      <TextInput source="comment" label="resources.revues.fields.comment" />
     </SimpleForm>
   </Create>
 );
