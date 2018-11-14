9a50e37ef06b45a31036077735d4e2da98cf58d7
Math <mathieu.risakota@gmail.com>
2018-11-14 15:29:26 +0100

    Add fields FR.JS
    Auto stash before merge of "react" and "origin/react"
    Merge remote-tracking branch 'origin/react' into react

--- src/views/AccountsFedeInserm.js
+++ src/views/AccountsFedeInserm.js
@@ -12,6 +12,7 @@ import {
   SingleFieldList,
   ChipField,
   TextField,
+  EmailField,
   DateField,
   BooleanField,
   TextInput,
@@ -36,11 +37,64 @@ const AccountsFedeInsermFilter = props => (
 export const AccountsFedeInsermList = props => (
   <List {...props} filters={<AccountsFedeInsermFilter />} perPage={10}>
     <Datagrid>
-      <LinkEdit source="uid" label="resources.janusAccounts.fields.uid" />
-      <LinkEdit source="mail" label="resources.janusAccounts.fields.mail" />
+      <LinkEdit
+        source="firstname"
+        label="resources.fedeInserm.fields.firstname"
+      />
+      <EmailField
+        source="inserm_email"
+        label="resources.fedeInserm.fields.inserm_email"
+      />
+      <EmailField source="email" label="resources.fedeInserm.fields.email" />
+      <LinkEdit
+        source="structure_code"
+        label="resources.fedeInserm.fields.structure_code"
+      />
+      <LinkEdit
+        source="team_number"
+        label="resources.fedeInserm.fields.team_number"
+      />
+      <LinkEdit
+        source="team_number"
+        label="resources.fedeInserm.fields.team_number"
+      />
+      <LinkEdit
+        source="team_name"
+        label="resources.fedeInserm.fields.team_name"
+      />
+      <LinkEdit
+        source="second_team_code"
+        label="resources.fedeInserm.fields.second_team_code"
+      />
+      <ReferenceField
+        label="resources.structures.fields.regional_delegation"
+        source="regional_delegation"
+        reference="regionals_delegations"
+        linkType="show"
+      >
+        <TextField source="code" />
+      </ReferenceField>
+      <TextField source="site" label="resources.fedeInserm.fields.site" />
+      <TextField source="city" label="resources.fedeInserm.fields.city" />
+      <TextField
+        source="membership"
+        label="resources.fedeInserm.fields.membership"
+      />
+      <TextField
+        source="type_of_assigned_structure"
+        label="resources.fedeInserm.fields.type_of_assigned_structure"
+      />
+      <DateField
+        source="register_date"
+        label="resources.fedeInserm.fields.register_date"
+      />
+      <DateField
+        source="last_connection"
+        label="resources.fedeInserm.fields.last_connection"
+      />
       <BooleanField
         source="active"
-        label="resources.janusAccounts.fields.active"
+        label="resources.fedeInserm.fields.active"
       />
       <EditButton />
       <DeleteButtonWithConfirmation />
